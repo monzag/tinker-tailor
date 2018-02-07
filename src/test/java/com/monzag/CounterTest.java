37a56c5d930b9e4af8f8a266861f7f873aee699e
monzag <zaganiacz.m@gmail.com>
2018-02-07 11:30:38 +0100

    Add generateList method

--- src/test/java/com/monzag/CounterTest.java
+++ src/test/java/com/monzag/CounterTest.java
@@ -6,5 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
     
+    private ArrayList<Integer> generateList(Integer n) {
+        ArrayList<Integer> players = new ArrayList<>();
+        for (Integer i = 1; i < n ; i++) {
+            players.add(i);
+        }
+
+        return players;
+    }
 
 }
