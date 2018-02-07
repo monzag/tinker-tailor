57c5e5009398fa955fa748b27b5eac92d146da1a
monzag <zaganiacz.m@gmail.com>
2018-02-07 11:35:32 +0100

    Add setup method and counter field

--- src/test/java/com/monzag/CounterTest.java
+++ src/test/java/com/monzag/CounterTest.java
@@ -1,11 +1,22 @@
 package com.monzag;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CounterTest {
-    
+
+    private Counter<Integer> counter;
+
+    @BeforeEach
+    void setup() {
+        counter = new Counter<>();
+    }
+
+
     private ArrayList<Integer> generateList(Integer n) {
         ArrayList<Integer> players = new ArrayList<>();
         for (Integer i = 1; i < n ; i++) {
