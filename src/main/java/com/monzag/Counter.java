58949faaa83dafb6b5cad8322a35541637873bd4
monzag <zaganiacz.m@gmail.com>
2018-02-07 11:23:57 +0100

    Add countWinner method

--- src/main/java/com/monzag/Counter.java
+++ src/main/java/com/monzag/Counter.java
@@ -2,7 +2,17 @@ package com.monzag;
 
 import java.util.ArrayList;
 
-public class Counter {
+public class Counter<T> {
 
+//    k - counter
 
+    public T countWinner(ArrayList<T> players, Integer k) {
+        int indexToRemove = 0;
+        while (players.size() > 1) {
+            indexToRemove = (indexToRemove + k - 1) % players.size();
+            players.remove(indexToRemove);
+        }
+
+        return players.get(0);
+    }
 }
